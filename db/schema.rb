2981da6519353e34467d0f497bcc408e84a57a0c
Maria Groves <mariagrovesh@gmail.com>
2019-11-11 17:48:55 +0100

    added views to toilets controller

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,13 @@ ActiveRecord::Schema.define(version: 2019_11_11_150355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "toilets", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
