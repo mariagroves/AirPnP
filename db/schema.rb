c3fa6ac4e555f61b29fcbc4a5652dd74d952f17d
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-11-12 13:35:08 +0100

    Added a migration and added price column to toilets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_092731) do
+ActiveRecord::Schema.define(version: 2019_11_12_110831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_092731) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.bigint "owner_id"
+    t.integer "price"
     t.index ["owner_id"], name: "index_toilets_on_owner_id"
   end
 
