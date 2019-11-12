6759c3379c5b9dabbd9a0169fc9293722962ad17
Francois Andrivet <francoisandrivet@gmail.com>
2019-11-12 11:51:10 +0100

    irene is master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_175542) do
+ActiveRecord::Schema.define(version: 2019_11_12_092731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 2019_11_11_175542) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.bigint "owner_id"
     t.index ["owner_id"], name: "index_toilets_on_owner_id"
-    t.string "photo"
   end
 
   create_table "users", force: :cascade do |t|
