14a4a51be14771807337c7e959d9faf986cf289b
Maria Groves <mariagrovesh@gmail.com>
2019-11-12 12:56:49 +0100

    added validations to booking and toilet models

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -3,4 +3,8 @@ class Toilet < ApplicationRecord
   belongs_to :owner, class_name: 'User'
   has_many :bookings
   has_many :renters, through: :bookings, class_name: 'User'
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :photo, presence: true
 end
