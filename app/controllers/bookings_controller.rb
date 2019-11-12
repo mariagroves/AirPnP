b1f7ee09143ee8da9dad28c454b0d87818bbb7ca
Maria Groves <mariagrovesh@gmail.com>
2019-11-12 14:37:50 +0100

    added booking controller, added method to booking model, added dashboard route

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,23 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @toilet = Toilet.find(params[:toilet_id])
+    @booking.renter = current_user
+    @booking.toilet = @toilet
+    if @booking.save
+      redirect_to dashboard_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :time)
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :renter, class_name: 'User'
   belongs_to :toilet
 
-  validates_uniqueness_of :renter_id, scope: :toilet_id
+  def owner
+    @toilet = self.toilet
+    return @toilet.owner
+  end
 end
