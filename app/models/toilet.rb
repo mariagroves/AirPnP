e99b486b580c598c06dee5b569891aa9bd6f9b5b
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-11-14 10:18:07 +0100

    replaced address with location in toilet model

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,6 +1,6 @@
 class Toilet < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :owner, class_name: 'User'
   has_many :bookings
   has_many :renters, through: :bookings, class_name: 'User'
